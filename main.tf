643559f13fb47b5f30e638343580a792828aa0ac
Werner Matias Kahnlein <matias.kahnlein@caylent.com>
2025-01-06 18:31:44 -0300

    feat: adding container cost bucket integration

--- main.tf
+++ main.tf
@@ -3,3 +3,7 @@ resource "nops_compute_copilot_integration" "integration" {
   region_name  = data.aws_region.current.id
   version      = local.module_version
 }
+
+resource "nops_container_cost_bucket" "container_cost_bucket" {
+  project_id = local.current_nops_project.id
+}
