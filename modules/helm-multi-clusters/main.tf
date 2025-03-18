3cef6a87ef46279137dacf2da7cd00b8ff08dac9
DJ Spatoulas <dspatoulas21@gmail.com>
2025-03-18 15:16:43 -0400

    feat: Add Windows compatibility

--- modules/helm-multi-clusters/main.tf
+++ modules/helm-multi-clusters/main.tf
@@ -14,6 +14,9 @@ data "http" "nops_clusters" {
 locals {
   clusters = jsondecode(data.http.nops_clusters.response_body)
   cluster  = [for c in local.clusters : c if c.name == var.cluster_name][0]
+
+  # Basic platform detection
+  is_windows = substr(pathexpand("~"), 0, 1) == "/" ? false : true
 }
 
 # Install Helm chart
@@ -27,65 +30,46 @@ resource "null_resource" "helm_upgrade_install" {
     helm_namespace    = var.helm_namespace
     cluster_arn       = data.aws_eks_cluster.cluster.arn
     cluster_id        = local.cluster.external_id
+    is_windows        = local.is_windows
   }
 
   provisioner "local-exec" {
-    command = <<-EOT
-      aws eks update-kubeconfig \
-        --name ${self.triggers.cluster_name} \
-        --region ${self.triggers.cluster_region} \
-        --kubeconfig ${self.triggers.kubeconfig}
-    EOT
+    command = "aws eks update-kubeconfig --name ${self.triggers.cluster_name} --region ${self.triggers.cluster_region} --kubeconfig ${self.triggers.kubeconfig}"
   }
 
   provisioner "local-exec" {
-    command = <<-EOT
-      helm upgrade -i ${var.helm_release_name} ${var.helm_repo} \
-        --namespace ${var.helm_namespace} \
-        ${var.create_namespace ? "--create-namespace" : ""} \
-        --kubeconfig ${self.triggers.kubeconfig} \
-        --set datadog.apiKey=${var.datadog_api_key} \
-        --set containerInsights.enabled=${var.container_insights_enabled} \
-        --set containerInsights.env_variables.APP_NOPS_K8S_AGENT_CLUSTER_ARN=${self.triggers.cluster_arn} \
-        --set containerInsights.env_variables.APP_AWS_S3_BUCKET=${var.s3_bucket_name} \
-        --set karpenops.enabled=${var.karpenops_enabled} \
-        --set karpenops.image.tag=${var.karpenops_image_tag} \
-        --set karpenops.clusterId=${self.triggers.cluster_id} \
-        --set nops.apiKey=${var.nops_api_token}
-    EOT
+    command = "helm upgrade -i ${var.helm_release_name} ${var.helm_repo} --namespace ${var.helm_namespace} ${var.create_namespace ? "--create-namespace" : ""} --kubeconfig ${self.triggers.kubeconfig} --set datadog.apiKey=${var.datadog_api_key} --set containerInsights.enabled=${var.container_insights_enabled} --set containerInsights.env_variables.APP_NOPS_K8S_AGENT_CLUSTER_ARN=${self.triggers.cluster_arn} --set containerInsights.env_variables.APP_AWS_S3_BUCKET=${var.s3_bucket_name} --set karpenops.enabled=${var.karpenops_enabled} --set karpenops.image.tag=${var.karpenops_image_tag} --set karpenops.clusterId=${self.triggers.cluster_id} --set nops.apiKey=${var.nops_api_token}"
   }
 
   provisioner "local-exec" {
-    command = <<-EOT
-      rm -f ${self.triggers.kubeconfig}
-    EOT
+    interpreter = local.is_windows ? ["PowerShell", "-Command"] : ["/bin/bash", "-c"]
+    command = (
+      local.is_windows
+      ? "if (Test-Path ${self.triggers.kubeconfig}) { Remove-Item -Force ${self.triggers.kubeconfig} }"
+      : "rm -f ${self.triggers.kubeconfig}"
+    )
   }
-
 
   # Uninstall Helm chart
   provisioner "local-exec" {
     when    = destroy
-    command = <<-EOT
-      aws eks update-kubeconfig \
-        --name ${self.triggers.cluster_name} \
-        --region ${self.triggers.cluster_region} \
-        --kubeconfig ${self.triggers.kubeconfig}
-    EOT
+    command = "aws eks update-kubeconfig --name ${self.triggers.cluster_name} --region ${self.triggers.cluster_region} --kubeconfig ${self.triggers.kubeconfig}"
   }
 
   provisioner "local-exec" {
-    when    = destroy
-    command = <<-EOT
-      helm uninstall ${self.triggers.helm_release_name} --namespace ${self.triggers.helm_namespace} --kubeconfig ${self.triggers.kubeconfig}
-    EOT
+    when       = destroy
+    command    = "helm uninstall ${self.triggers.helm_release_name} --namespace ${self.triggers.helm_namespace} --kubeconfig ${self.triggers.kubeconfig}"
+    on_failure = continue
   }
 
   provisioner "local-exec" {
-    when    = destroy
-    command = <<-EOT
-      rm -f ${self.triggers.kubeconfig}
-    EOT
+    when        = destroy
+    interpreter = self.triggers.is_windows ? ["PowerShell", "-Command"] : ["/bin/bash", "-c"]
+    command = (
+      self.triggers.is_windows
+      ? "if (Test-Path ${self.triggers.kubeconfig}) { Remove-Item -Force ${self.triggers.kubeconfig} }"
+      : "rm -f ${self.triggers.kubeconfig}"
+    )
+    on_failure = continue
   }
 }
-
-
